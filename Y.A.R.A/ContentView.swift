1943b6dc49057ccc0507ebaed5fff2d003625270
GichukiPaul <gichukipaul043@gmail.com>
2023-10-29 00:21:05 +0300

    Initial Commit

---
+++ Y.A.R.A/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Y.A.R.A
+//
+//  Created by user on 29/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Y.A.R.A/Y_A_R_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Y_A_R_AApp.swift
+//  Y.A.R.A
+//
+//  Created by user on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Y_A_R_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
